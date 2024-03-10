008b952630bb61024851fa76ca0dbbe01e82cc1a
Sombhu2022 <sombhudas93@gmail.com>
2024-03-10 18:45:05 +0530

    auth add

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../src/global/global.scss'
+import { Provider } from 'react-redux'
+import {store} from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
+  <Provider store={store}>
+
     <App />
+  </Provider>
 
 )
