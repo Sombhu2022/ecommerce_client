3e4e3e8edf77fb87d995c56cbd37e9a11f4e2db9
Sombhu das <105740734+Sombhu2022@users.noreply.github.com>
2024-04-12 10:26:22 +0530

    Update Order.jsx

--- src/pages/Order/Order.jsx
+++ src/pages/Order/Order.jsx
@@ -3,14 +3,14 @@ import axios from 'axios'
 import API from '../../utils/axiosSetup';
 import { baseUrl } from '../../App';
 // import fatch from 'fs'
-const api_key = "AIzaSyCYHwYUFrN0Mf3iMWvQdjqApC7W054QC-o"
+
 
 
 function Order() {
   
   const [latitude , setLatitude]=useState()
   const [longitude , setLongitude] = useState()
-  const url = `https://www.mapquestapi.com/geocoding/v1/reverse?key=${api_key}&location='+${latitude}+'%2C'+${longitude}+'&outFormat=json&thumbMaps=false`;
+
 
   const getLocation =async ()=>{
      if(navigator.geolocation){
