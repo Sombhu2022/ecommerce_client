811781473e820833f80d97041ebc1e3ec80e7f6a
Sombhu2022 <sombhudas93@gmail.com>
2024-07-28 01:58:49 +0530

    28 jul

--- src/App.jsx
+++ src/App.jsx
@@ -80,4 +80,5 @@ function App() {
 
 export default App;
 
-export const baseUrl = "http://localhost:8080";
+// export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://som-ecommerce-api.vercel.app";
